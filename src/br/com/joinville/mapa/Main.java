9549b0f817b71a6644a3d18f2d2226616d2df5ef
Andressa Costa <andressacosta@MacBook-Air-de-Andressa.local>
2025-03-14 13:03:46 -0300

    task-1-main

---
+++ src/br/com/joinville/mapa/Main.java
@@ -0,0 +1,7 @@
+package br.com.joinville.mapa;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao Mapeamento de Áreas Verdes de Joinville!");
+    }
+}
